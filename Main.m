278b9ae56f7d01f973d47cfe3795ec60331ad39d
Rodrigo Brechard <rodrigobrechard@gmail.com>
2018-11-05 15:59:26 +0100

    output added and lambda changed

--- Main.m
+++ Main.m
@@ -11,6 +11,7 @@ X=[[1 0 0 0 0 0 0 0]
     [0 0 0 0 0 0 0 1]];
 
 Y = X;
+output = X;
 
 %h_Wb=f(W'.*X);
 [n_nodes_in, n_samples] = size(X);
@@ -20,8 +21,8 @@ nodes_in_1 = 8;
 nodes_hid_2 = 3;
 nodes_out_3 = 8;
 
-alpha = 0.1;
-lambda = 0.001;
+alpha = 0.9;
+lambda = 0.0001;
 
 W_1 = normrnd(0, 0.01, nodes_hid_2, nodes_in_1);
 b_1 = normrnd(0, 0.01, nodes_hid_2, 1);
@@ -36,13 +37,14 @@ D_W_2 = zeros(nodes_out_3, nodes_hid_2);
 D_b_2 = zeros(nodes_out_3, 1);
 
 c = 0;
-
-for i = 1:100000
+delta_3 = 1;
+while max(abs(delta_3)) > 0.05
     D_W_1 = zeros(nodes_hid_2, nodes_in_1);
     D_b_1 = zeros(nodes_hid_2, 1);
 
     D_W_2 = zeros(nodes_out_3, nodes_hid_2);
     D_b_2 = zeros(nodes_out_3, 1);
+    output = X;
     for sample = 1:n_samples
         c = c + 1;
 % Perform the backpropagation algorithm
@@ -56,6 +58,7 @@ for i = 1:100000
         z_3 = W_2 * a_2 + b_2;
         a_3 = 1 ./ (1+exp(-(z_3)));
         
+        output(:, sample) = a_3;
     % Error of the last layer
         delta_3 = -(Y(:, sample) - a_3);
 
@@ -76,7 +79,7 @@ for i = 1:100000
         D_W_2 = D_W_2 + J_W_2;
         D_b_2 = D_b_2 + J_b_2;
 
-        errors(c) = std(delta_3);
+        errors(c) = max(delta_3);
 
     end
 % Update the parameters
